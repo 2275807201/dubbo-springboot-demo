892e740abd7008c839db1d07e0a5d365903ba662
lianghuan <2275807201@qq.com>
2019-07-05 19:04:39 +0800

    解决jar包冲突问题

--- dubbo-consumer/src/main/java/com/liangh/dubbo/demo/consumer/DubboDemoConsumerApplication.java
+++ dubbo-consumer/src/main/java/com/liangh/dubbo/demo/consumer/DubboDemoConsumerApplication.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
